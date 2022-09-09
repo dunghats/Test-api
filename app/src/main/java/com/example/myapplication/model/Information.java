bd3c19dc2837628e121308a83adfffb4c3a3cf6a
daicaset <dunghaph13370>
2022-09-09 08:50:07 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/model/Information.java
@@ -0,0 +1,52 @@
+package com.example.myapplication.model;
+
+public class Information {
+    private String id;
+    private String url;
+    private int width;
+    private int height;
+    private String weight_imperial;
+    private String getWeight_metric;
+    private String breeds_id;
+    private String breeds_name;
+    private String cfa_url;
+    private String vetstreet_url;
+    private String vcahospitals_url;
+    private String temperament;
+    private String origin;
+    private String country_codes;
+    private String country_code;
+    private String description;
+    private String life_span;
+    private int indoor;
+    private int lap;
+    private int adaptability;
+    private int affection_level;
+    private int child_friendly;
+    private int cat_friendly;
+    private int dog_friendly;
+    private int energy_level;
+    private int grooming;
+    private int health_issues;
+    private int intelligence;
+    private int shedding_level;
+    private int social_needs;
+    private int stranger_friendly;
+    private int vocalisation;
+    private int bidability;
+    private int experimental;
+    private int hairless;
+    private int natural;
+    private int rare;
+    private int rex;
+    private int suppressed_tail;
+    private int short_legs;
+    private String wikipedia_url;
+    private int hypoallergenic;
+    private String reference_image_id;
+
+
+
+
+
+}
